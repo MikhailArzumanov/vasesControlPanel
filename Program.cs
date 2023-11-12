5d3b5bc322e36f63b9b2ed94b62deebbd2b4bfcc
Mikhail <mikhailArzumanov@yandex.ru>
2023-11-12 12:13:15 +0300

    Были добавлены общие файлы

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using vaseApi.Data;
+
+namespace vaseApi {
+    public class Program {
+        public static void Main(string[] args) {
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+                try {
+                    var context = services.GetRequiredService<ApplicationContext>();
+                    context.Database.Migrate();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex) {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>()
+                .UseUrls("http://185.173.157.85:1313", "http://localhost:1313");
+    }
+}
---
+++ Utils/TokenHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace vaseApi.Utils {
+    public class TokenHandler {
+        public static string BuildToken(string[] roles, IConfiguration config) {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var token = new JwtSecurityToken(
+                expires: DateTime.Now.AddMinutes(Double.Parse(config["Jwt:LifeTime"])), 
+                signingCredentials: creds);
+            
+            token.Payload["roles"] = roles;
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
