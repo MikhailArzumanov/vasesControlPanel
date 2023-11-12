57a1e1aa5953596b882411415b5341a78f0d035f
Mikhail <mikhailArzumanov@yandex.ru>
2023-11-12 12:12:54 +0300

    Миграции были добавлены

---
+++ Migrations/20231027041115_FirstSetOfModels.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace vaseApi.Migrations {
+    /// <inheritdoc />
+    public partial class FirstSetOfModels : Migration {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.CreateTable(
+                name: "Colors",
+                columns: table => new {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Commentary = table.Column<string>(type: "text", nullable: false),
+                    HexRepresentation = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_Colors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genuses",
+                columns: table => new {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    HtmlMarkup = table.Column<string>(type: "text", nullable: false),
+                    MarkdownText = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_Genuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vases",
+                columns: table => new {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Commentary = table.Column<string>(type: "text", nullable: true),
+                    LastWatering = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    WateredLevel = table.Column<double>(type: "double precision", nullable: false),
+                    MainColorId = table.Column<int>(type: "integer", nullable: false),
+                    SideColorId = table.Column<int>(type: "integer", nullable: false),
+                    FlowerGenusId = table.Column<int>(type: "integer", nullable: false),
+                    VaseCondition = table.Column<int>(type: "integer", nullable: false),
+                    VaseWeight = table.Column<double>(type: "double precision", nullable: false),
+                    LastRepairDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MountingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table => {
+                    table.PrimaryKey("PK_Vases", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vases_Colors_MainColorId",
+                        column: x => x.MainColorId,
+                        principalTable: "Colors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vases_Colors_SideColorId",
+                        column: x => x.SideColorId,
+                        principalTable: "Colors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Vases_Genuses_FlowerGenusId",
+                        column: x => x.FlowerGenusId,
+                        principalTable: "Genuses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vases_FlowerGenusId",
+                table: "Vases",
+                column: "FlowerGenusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vases_MainColorId",
+                table: "Vases",
+                column: "MainColorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vases_SideColorId",
+                table: "Vases",
+                column: "SideColorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropTable(
+                name: "Vases");
+
+            migrationBuilder.DropTable(
+                name: "Colors");
+
+            migrationBuilder.DropTable(
+                name: "Genuses");
+        }
+    }
+}
---
+++ Migrations/20231110032553_ModelsChanges.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vaseApi.Migrations {
+    /// <inheritdoc />
+    public partial class ModelsChanges : Migration {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder) {
+            migrationBuilder.AddColumn<bool>(
+                name: "Autofertilizeing",
+                table: "Vases",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Autowatering",
+                table: "Vases",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<double>(
+                name: "FertilizingIntensity",
+                table: "Vases",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "GlassOpacity",
+                table: "Vases",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLightingToggled",
+                table: "Vases",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<double>(
+                name: "LightingIntensity",
+                table: "Vases",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "WateringIntensity",
+                table: "Vases",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropColumn(
+                name: "Autofertilizeing",
+                table: "Vases");
+
+            migrationBuilder.DropColumn(
+                name: "Autowatering",
+                table: "Vases");
+
+            migrationBuilder.DropColumn(
+                name: "FertilizingIntensity",
+                table: "Vases");
+
+            migrationBuilder.DropColumn(
+                name: "GlassOpacity",
+                table: "Vases");
+
+            migrationBuilder.DropColumn(
+                name: "IsLightingToggled",
+                table: "Vases");
+
+            migrationBuilder.DropColumn(
+                name: "LightingIntensity",
+                table: "Vases");
+
+            migrationBuilder.DropColumn(
+                name: "WateringIntensity",
+                table: "Vases");
+        }
+    }
+}
